23dcfe233c9f457b5c956e4e6510bae0ff6bd646
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-09-15 00:27:51 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-
-    if storage_type == 'db':
-        __tablename__ = 'reviews'
-
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        text = Column(String(1024), nullable=False)
-
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
-        place_id = ""
-        user_id = ""
-        text = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
