3a00526e1c495e05dcadbe006fd00f3e541169df
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-10-23 19:41:27 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-"""Create and distribute an archive to the servers,
-    using function deploy
-"""
-from fabric.api import put, run, env
-from os import path
+#  Fabric script that generates a .tgz archive from the
+# contents of the web_static folder of your AirBnB Clone repo
+# using the function do_pack
+import os
+from fabric.api import run, put, env
 
-env.user = 'ubuntu'
-env.hosts = ['35.229.108.96', '35.196.60.22']
+env.hosts = ['100.25.102.250', '54.160.118.16']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """function to deploy an archive to the servers"""
-    if path.exists(archive_path) is False:
+    """Create a tar gzipped archive of the directory web_static."""
+    if os.path.exists(archive_path) is False:
         return False
-
-    archive = archive_path.split('/')[1]
-    folder = archive[:-4]
-
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(folder))
-        run("tar -xzf /tmp/{} -C \
-            /data/web_static/releases/{}".format(archive, folder))
-        run("rm /tmp/{}".format(archive))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}".format(folder, folder))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} \
-            /data/web_static/current".format(folder))
-        print('New release deployed!')
-        return True
-    except:
-        return False
+    else:
+        try:
+            put(archive_path, "/tmp/")
+            """ putting the file to .tgz """
+            file_name = archive_path.split("/")[1]
+            """ splitting .tgz """
+            file_name2 = file_name.split(".")[0]
+            """ spliting archivo """
+            final_name = "/data/web_static/releases/" + file_name2 + "/"
+            run("mkdir -p " + final_name)
+            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
+            run("rm /tmp/" + file_name)
+            run("mv " + final_name + "web_static/* " + final_name)
+            run("rm -rf " + final_name + "web_static")
+            run("rm -rf /data/web_static/current")
+            run("ln -s " + final_name + " /data/web_static/current")
+            print("New version deployed!")
+            return True
+        except Exception:
+            return False
