9c23338915ff06a8f540928d92cfddccf5df8e0c
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-10-23 18:56:12 +0300

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '57 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+   </body>
+  </html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
